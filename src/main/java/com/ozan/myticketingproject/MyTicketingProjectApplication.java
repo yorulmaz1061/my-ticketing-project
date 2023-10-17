0ce298b35bf8b0e005c7f3ea8e2449401dac09d0
yorulmaz1061 <ozan.yorulmaz@gmail.com>
2023-10-17 11:34:09 +0300

    initial commit

---
+++ src/main/java/com/ozan/myticketingproject/MyTicketingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ozan.myticketingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTicketingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTicketingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ozan/myticketingproject/MyTicketingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozan.myticketingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTicketingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
